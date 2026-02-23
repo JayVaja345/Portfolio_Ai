83580187c3b9c922bce027fde8f4ebd676d35778
Jay Vaja <vajajay325@gmail.com>
2026-02-23 17:06:19 +0530

    fix: added required OpenRouter headers changes

--- backend/app/services/openrouter_service.py
+++ backend/app/services/openrouter_service.py
@@ -6,22 +6,22 @@ from ..resume_context import RESUME_CONTEXT
 def ask_ai(question: str):
     try:
         response = requests.post(
-        "https://openrouter.ai/api/v1/chat/completions",
-        headers={
-            "Authorization": f"Bearer {settings.OPENROUTER_API_KEY}",
-            "Content-Type": "application/json",
-            "HTTP-Referer": "https://portfolio-ai-m6w2.onrender.com",
-            "X-Title": "AI Portfolio",
-        },
-        json={
-            "model": "mistralai/mistral-7b-instruct",
-            "messages": [
-                {"role": "system", "content": RESUME_CONTEXT},
-                {"role": "user", "content": question}
-            ]
-        },
-        timeout=30
-    )
+            "https://openrouter.ai/api/v1/chat/completions",
+            headers={
+                "Authorization": f"Bearer {settings.OPENROUTER_API_KEY}",
+                "Content-Type": "application/json",
+                "HTTP-Referer": "https://portfolio-ai-m6w2.onrender.com",
+                "X-Title": "AI Portfolio",
+            },
+            json={
+                "model": "mistralai/mistral-7b-instruct",
+                "messages": [
+                    {"role": "system", "content": RESUME_CONTEXT},
+                    {"role": "user", "content": question}
+                ]
+            },
+            timeout=30
+        )
 
         print("STATUS:", response.status_code)
         print("BODY:", response.text)
@@ -33,4 +33,4 @@ def ask_ai(question: str):
 
     except requests.exceptions.RequestException as e:
         print("🔥 OpenRouter Error:", e)
-    raise HTTPException(status_code=500, detail=str(e))
+        raise HTTPException(status_code=500, detail=str(e))
