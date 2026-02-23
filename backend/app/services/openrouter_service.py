e4b47d8c8485e20e3cfd0a746c6b7b832c1319ca
Jay Vaja <vajajay325@gmail.com>
2026-02-23 16:41:23 +0530

    fix: added required OpenRouter headers to resolve 401 unauthorized

--- backend/app/services/openrouter_service.py
+++ backend/app/services/openrouter_service.py
@@ -5,21 +5,22 @@ from ..resume_context import RESUME_CONTEXT
 
 def ask_ai(question: str):
     try:
-        print("API KEY:", settings.OPENROUTER_API_KEY)
         response = requests.post(
-            "https://openrouter.ai/api/v1/chat/completions",
-            headers={
-                "Authorization": f"Bearer {settings.OPENROUTER_API_KEY}",
-                "Content-Type": "application/json",
-            },
-            json={
-                "model": "mistralai/mistral-7b-instruct",
-                "messages": [
-                    {"role": "system", "content": RESUME_CONTEXT},
-                    {"role": "user", "content": question}
-                ]
-            },
-            timeout=30
+    "https://openrouter.ai/api/v1/chat/completions",
+    headers={
+        "Authorization": f"Bearer {settings.OPENROUTER_API_KEY}",
+        "Content-Type": "application/json",
+        "HTTP-Referer": "https://portfolio-ai-m6w2.onrender.com",
+        "X-Title": "AI Portfolio",
+    },
+    json={
+        "model": "mistralai/mistral-7b-instruct",
+        "messages": [
+            {"role": "system", "content": RESUME_CONTEXT},
+            {"role": "user", "content": question}
+        ]
+    },
+    timeout=30
         )
 
         response.raise_for_status()
